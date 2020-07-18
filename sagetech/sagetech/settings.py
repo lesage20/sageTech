bcd158361edd1d4b85f00d0278b8a03bf4889417
angezanou00@gmail.com0 <angezanou00@gmail.com>
2020-07-18 02:47:29 +0200

    ajout de gitignore

--- sagetech/sagetech/settings.py
+++ sagetech/sagetech/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# my imports
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -132,3 +135,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, '../static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, '../media_cdn')
+
+#heroku part
+
+django_heroku.settings(locals())
